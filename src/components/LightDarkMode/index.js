a3681620977cb8bda70223ecc6755981c8322ccb
deepika <deepikadasari78@gmail.com>
2023-04-26 17:00:38 +0000

    Solution Of lightDarkMode

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -4,6 +4,7 @@ import './index.css'
 
 class LightDarkMode extends Component {
   state = {isDarkMode: true}
+
   changeMode = () => {
     const {isDarkMode} = this.state
     this.setState({isDarkMode: !isDarkMode})
@@ -11,20 +12,20 @@ class LightDarkMode extends Component {
 
   render() {
     let modeCls
-    
+    const {isDarkMode} = this.state
     const buttonText = isDarkMode
       ? ((modeCls = 'dark-mode'), 'Light Mode')
       : ((modeCls = 'light-mode'), 'Dark Mode')
-      
-  return ( 
-    <div className="container">
-      <div {modeCls}>
-        <h1 className="mode-heading">Click To Change Mode</h1>
-        <button className="mode-btn" onClick={this.changeMode} type="button">
+
+    return (
+      <div className="container-1">
+        <div className={`container ${modeCls}`}>
+          <h1 className="mode-heading">Click To Change Mode</h1>
+          <button className="mode-btn" onClick={this.changeMode} type="button">
             {buttonText}
-        </button>
+          </button>
+        </div>
       </div>
-  </div> 
     )
   }
 }
